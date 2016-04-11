6c19178f833d1605ba21e80cf60cf166b1e2eacf
Barbar <bbk.barbar@gmail.com>
2016-04-11 06:41:21 +0200

    Minimal update

--- LED_display/led_display_4x7.c
+++ LED_display/led_display_4x7.c
@@ -25,9 +25,39 @@
 #define PORT_OF_SEGM_G         
 #define PORT_OF_SEGM_H         
 #define PORT_OF_SEGM_DOT       PORT_OF_SEGM_H
-#define PORT_OF_ 
+
+#define PORT_OF_DIGIT_1         
+#define PORT_OF_DIGIT_2         
+#define PORT_OF_DIGIT_3         
+#define PORT_OF_DIGIT_4         
 
 
-uint8_t res;
+#define PIN_OF_SEGM_A          
+#define PIN_OF_SEGM_B          
+#define PIN_OF_SEGM_C          
+#define PIN_OF_SEGM_D          
+#define PIN_OF_SEGM_E          
+#define PIN_OF_SEGM_F          
+#define PIN_OF_SEGM_G          
+#define PIN_OF_SEGM_H          
+#define PIN_OF_SEGM_DOT        
+
+#define PIN_OF_DIGIT_1         
+#define PIN_OF_DIGIT_2         
+#define PIN_OF_DIGIT_3         
+#define PIN_OF_DIGIT_4 
+
+
+// Used to store states of segments in one variable
+//                       ABCDEFGH             
+uint8_t segmentValue = 0b00000000;
+
+void setStateOfSegments(uint8_t value){
+    //TODO
+    if( value & 0b1000000 ){ //A
+        
+    }
+}
+
 unsigned char a;
  
